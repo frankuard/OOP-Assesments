83fe80ea1ffdd413a34fe4208f876307ed946c11
Roshan <roshankarki4956@gmail.com>
2026-04-01 23:47:33 +0545

    Adding week 4 files

--- Week 4/week4/src/week4/question8Logic.java
+++ Week 4/week4/src/week4/question8Logic.java
@@ -26,12 +26,14 @@ public class question8Logic {
 				scanner.nextLine();
 				
 				while (true) {
+					
+					// NAME
 				System.out.println("Please enter your full name:");
 				 name = scanner.nextLine();
 				int len = name.length();
 				
 				if (len<4) {
-					System.out.println("The name musrt be greater than 4");
+					System.out.println("The name must be greater than 4");
 					continue;
 				}
 				
@@ -41,34 +43,75 @@ public class question8Logic {
 				}
 				
 				while (true) {
+					
+					// PHONE NUMBER
+					
 				System.out.println("Please enter your phone number:");
 				number = scanner.nextLine();
 				
 				int len2= number.length();
 				
-				if (len2!=10) {
-					System.out.println("The total digit should be 10");
+				if (len2!=10 || !number.startsWith("0")) {
+					System.out.println("The total digit should be 10 and start with 0");
 					continue;
 				}
 				break;
 				}
 				
 				while (true) {
+					
+					// AGE
+				
 				System.out.println("Please enter your date of birth:");
 				dob = scanner.nextLine();
 				
 				int len3 = dob.length();
 				
+				if (!dob.matches("\\d{2}/\\d{2}/\\d{4}")) {
+					System.out.println("Invalid DOB Format");
+					continue ;
 				}
+				int year = Integer.parseInt(dob.substring(6));
+				int age = 2026 - year;
+				
+				if (age < 21) {
+					
+				System.out.println("You must be at least 21 years old");
+				continue;
+				}
+				break;
+				
+				}
+				while (true) {
 				System.out.println("Please enter your password:");
 				password = scanner.nextLine();
 				
+				if (!password.matches("[A-Za-z]+[@&][0-9]+")) {
+					System.out.println("Password must start with letters, contain @ or &, and end with numbers");
+					continue;
+				
+				}
+				
+				System.out.println("Please confirm your password");
+				String confirmPass = scanner.nextLine();
+				
+				
+				if (!password.equals(confirmPass)) {
+					System.out.println("Password do not match");
+					continue;
+				}
+				
+				break;
+				
+				}
+				
+				// SUCCESS
+				
 				userSignUp user = new userSignUp(name,number,dob, password);
-				
-				
+				System.out.println("You have successfully signed up.");
 			
 			}
-			if (choice==2) {
+			else if (choice==2) {
 				System.out.println("Thank you for using the Application.");
 				break;
 			}
